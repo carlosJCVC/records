8ae18804285a64b2e362923140665ed3f3aefe5f
Carlos <carlosveizaga.jcvc@gmail.com>
2019-06-25 15:49:46 -0400

    fixed bug of password and user incorrect

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const env = require('../../env.json');
 
 //connecting to database
 //const string_connection = `mongodb://${ env.HOST }/${ env.DATABASE_NAME }`;
-const string_connection = `mongodb://${ env.user_database }:${ env.password_database }@ds243317.mlab.com:43317/${ env.database_name }`;
+const string_connection = `mongodb://${ env.DATABASE_USER }:${ env.DATABASE_PASSWORD }@ds243317.mlab.com:43317/${ env.DATABASE_NAME }`;
 
 mongoose.connect(string_connection, {
     useCreateIndex: true,
